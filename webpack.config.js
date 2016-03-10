c5656f51ed33c98984a965e5a2d3611136e6cc93
Michael Jackson <mjijackson@gmail.com>
2016-03-10 12:46:05 -0800

    Strip invariants in production build

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,12 @@ module.exports = {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
     ]
-  }
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ]
 
 }
