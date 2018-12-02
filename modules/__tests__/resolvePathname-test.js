20152400021377da9aa5e25fc0846ebe2d17fdcb
Michael Jackson <michael@jackson.us>
2018-12-02 12:01:25 -0800

    Use Rollup for builds, Jest for testing

--- modules/__tests__/resolvePathname-test.js
+++ modules/__tests__/resolvePathname-test.js
@@ -1,38 +1,37 @@
-import expect from 'expect'
-import resolvePathname from '../index'
+import resolvePathname from 'resolve-pathname';
 
 describe('resolvePathname', () => {
   it('works when from is not given', () => {
-    expect(resolvePathname('c')).toEqual('c')
-  })
+    expect(resolvePathname('c')).toEqual('c');
+  });
 
   it('works when from is relative', () => {
-    expect(resolvePathname('c', 'a/b')).toEqual('a/c')
-  })
+    expect(resolvePathname('c', 'a/b')).toEqual('a/c');
+  });
 
   it('works when to is absolute', () => {
-    expect(resolvePathname('/c', '/a/b')).toEqual('/c')
-  })
+    expect(resolvePathname('/c', '/a/b')).toEqual('/c');
+  });
 
   it('works when to is empty', () => {
-    expect(resolvePathname('', '/a/b')).toEqual('/a/b')
-  })
+    expect(resolvePathname('', '/a/b')).toEqual('/a/b');
+  });
 
   it('works when to is a sibling of the parent', () => {
-    expect(resolvePathname('../c', '/a/b')).toEqual('/c')
-  })
+    expect(resolvePathname('../c', '/a/b')).toEqual('/c');
+  });
 
   it('works when to is a sibling path', () => {
-    expect(resolvePathname('c', '/a/b')).toEqual('/a/c')
-  })
+    expect(resolvePathname('c', '/a/b')).toEqual('/a/c');
+  });
 
   it('works when from is an index path', () => {
-    expect(resolvePathname('c', '/a/')).toEqual('/a/c')
-  })
+    expect(resolvePathname('c', '/a/')).toEqual('/a/c');
+  });
 
   it('works when to points to the parent directory', () => {
-    expect(resolvePathname('..', '/a/b')).toEqual('/')
-  })
+    expect(resolvePathname('..', '/a/b')).toEqual('/');
+  });
 
   // Copied from node's test/parallel/test-url.js
   const nodeURLResolveTestCases = [
@@ -77,14 +76,14 @@ describe('resolvePathname', () => {
     //['http://example.com/b//c//d;p?q#blarg',
     //'http:/a/b/c/d',
     //'http://example.com/a/b/c/d'],
-    ['/foo/bar/baz', '/../etc/passwd', '/etc/passwd'],
+    ['/foo/bar/baz', '/../etc/passwd', '/etc/passwd']
     //['http://localhost', 'file:///Users/foo', 'file:///Users/foo'],
     //['http://localhost', 'file://foo/Users', 'file://foo/Users']
-  ]
+  ];
 
   nodeURLResolveTestCases.forEach(([from, to, expected]) => {
     it(`resolvePathname('${to}', '${from}') == '${expected}'`, () => {
-      expect(resolvePathname(to, from)).toEqual(expected)
-    })
-  })
-})
+      expect(resolvePathname(to, from)).toEqual(expected);
+    });
+  });
+});
--- modules/index.js
+++ modules/index.js
@@ -1,73 +1,75 @@
 function isAbsolute(pathname) {
-  return pathname.charAt(0) === '/'
+  return pathname.charAt(0) === '/';
 }
 
 // About 1.5x faster than the two-arg version of Array#splice()
 function spliceOne(list, index) {
-  for (let i = index, k = i + 1, n = list.length; k < n; i += 1, k += 1) {
-    list[i] = list[k]
+  for (var i = index, k = i + 1, n = list.length; k < n; i += 1, k += 1) {
+    list[i] = list[k];
   }
 
-  list.pop()
+  list.pop();
 }
 
 // This implementation is based heavily on node's url.parse
-function resolvePathname(to, from = '') {
-  const toParts = to && to.split('/') || []
-  let fromParts = from && from.split('/') || []
+function resolvePathname(to, from) {
+  if (from === undefined) from = '';
 
-  const isToAbs = to && isAbsolute(to)
-  const isFromAbs = from && isAbsolute(from)
-  const mustEndAbs = isToAbs || isFromAbs
+  var toParts = (to && to.split('/')) || [];
+  var fromParts = (from && from.split('/')) || [];
+
+  var isToAbs = to && isAbsolute(to);
+  var isFromAbs = from && isAbsolute(from);
+  var mustEndAbs = isToAbs || isFromAbs;
 
   if (to && isAbsolute(to)) {
     // to is absolute
-    fromParts = toParts
+    fromParts = toParts;
   } else if (toParts.length) {
     // to is relative, drop the filename
-    fromParts.pop()
-    fromParts = fromParts.concat(toParts)
+    fromParts.pop();
+    fromParts = fromParts.concat(toParts);
   }
 
-  if (!fromParts.length)
-    return '/'
+  if (!fromParts.length) return '/';
 
-  let hasTrailingSlash
+  var hasTrailingSlash;
   if (fromParts.length) {
-    const last = fromParts[fromParts.length - 1]
-    hasTrailingSlash = (last === '.' || last === '..' || last === '')
+    var last = fromParts[fromParts.length - 1];
+    hasTrailingSlash = last === '.' || last === '..' || last === '';
   } else {
-    hasTrailingSlash = false
+    hasTrailingSlash = false;
   }
 
-  let up = 0
-  for (let i = fromParts.length; i >= 0; i--) {
-    const part = fromParts[i]
+  var up = 0;
+  for (var i = fromParts.length; i >= 0; i--) {
+    var part = fromParts[i];
 
     if (part === '.') {
-      spliceOne(fromParts, i)
+      spliceOne(fromParts, i);
     } else if (part === '..') {
-      spliceOne(fromParts, i)
-      up++
+      spliceOne(fromParts, i);
+      up++;
     } else if (up) {
-      spliceOne(fromParts, i)
-      up--
+      spliceOne(fromParts, i);
+      up--;
     }
   }
 
-  if (!mustEndAbs)
-    for (; up--; up)
-      fromParts.unshift('..')
+  if (!mustEndAbs) for (; up--; up) fromParts.unshift('..');
 
-  if (mustEndAbs && fromParts[0] !== '' && (!fromParts[0] || !isAbsolute(fromParts[0])))
-    fromParts.unshift('')
+  if (
+    mustEndAbs &&
+    fromParts[0] !== '' &&
+    (!fromParts[0] || !isAbsolute(fromParts[0]))
+  )
+    fromParts.unshift('');
 
-  let result = fromParts.join('/')
+  var result = fromParts.join('/');
 
-  if (hasTrailingSlash && result.substr(-1) !== '/')
-    result += '/'
+  if (hasTrailingSlash && result.substr(-1) !== '/') result += '/';
 
-  return result
+  return result;
 }
 
-export default resolvePathname
+export default resolvePathname;
